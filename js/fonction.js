09112aaa938e356ab3e46dd4425dfeff2fd3bb2a
alexandre-laine <118631107+alexandre-laine@users.noreply.github.com>
2024-09-02 19:00:10 +0200

    contact page

---
+++ js/fonction.js
@@ -0,0 +1,13 @@
+function take_message() {
+                    
+    var t = document.getElementById('message_to_send').value;
+
+    alert(t)
+
+}
+
+function open_txt() {
+
+    alert(Date.call())
+    
+}
